44d9200d01a71b90e4e3d7107b17f8581e93e55a
Jimmy Yeung <jiyeung@ebay.com>
2020-07-06 18:34:58 +0800

    Initial Commit

---
+++ QuickTrack/QuickTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickTrackApp.swift
+//  QuickTrack
+//
+//  Created by Yeung, Jimmy(AWF) on 6/7/2020.
+//
+
+import SwiftUI
+
+@main
+struct QuickTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
